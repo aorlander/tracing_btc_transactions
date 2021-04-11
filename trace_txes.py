35411b19a0f3589dd18e4235a73bb6a3dc4a597f
aorlander <aborlander@gmail.com>
2021-04-11 14:56:43 -0400

    initial commit

--- trace_txes.py
+++ trace_txes.py
@@ -13,12 +13,12 @@ rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpc_user, rpc_password, 
 
 class TXO:
     def __init__(self, tx_hash, n, amount, owner, time ):
-        self.tx_hash = tx_hash 
-        self.n = n
-        self.amount = amount
-        self.owner = owner
-        self.time = time
-        self.inputs = []
+        self.tx_hash = tx_hash   # (string) the tx_hash on the Bitcoin blockchain
+        self.n = n               # (int) the position of this output in the transaction
+        self.amount = amount     # (int) the value of this transaction output (in Satoshi)
+        self.owner = owner       # (string) the Bitcoin address of the owner of this output
+        self.time = time         # (Datetime) the time of this transaction as a datetime object
+        self.inputs = []         # (TXO[]) a list of TXO objects
 
     def __str__(self, level=0):
         ret = "\t"*level+repr(self.tx_hash)+"\n"
@@ -36,11 +36,21 @@ class TXO:
         return json.dumps(json_dict, sort_keys=True, indent=4)
 
     @classmethod
+    # from_tx_hash(self,tx_hash,n) - this classmethod should connect to the Bitcoin blockchain, and retrieve the nth output 
+    # of the transaction with the given hash. Then it should create a new object with the fields, 'tx_hash’, 'n’, 'amount’, 
+    # ‘owner’ and ‘time’ set to the values retrieved from the blockchain. This method does not need to initialize the list 
+    # 'inputs’. Note that the ‘time’ field should be converted to a datetime object (using the datetime.fromtimestamp method)
     def from_tx_hash(cls,tx_hash,n=0):
         pass
+        tx = rpc_connection.getrawtransaction(tx_hash,True)
+
         #YOUR CODE HERE
 
+    # get_inputs(self,depth) - this method should connect to the Bitcoin blockchain, and populate the list of inputs, 
+    # up to a depth d. In other words, if d=1  it should create TXO objects to populate self.inputs with the appropriate 
+    # TXO objects. If d=2  it should also populate the inputs field of each of the TXOs in self.inputs etc.
     def get_inputs(self,d=1):
         pass
         #YOUR CODE HERE
+        print(self)
 
